84228162afbcdadce3611d086be2e0ab836e79cf
Anubhavbaranwal <anubhavbaranwal08@gmail.com>
2024-07-23 22:27:17 +0530

    feat: Add CORS configuration for frontend and backend
    This commit adds CORS configuration to the backend server in the Backend/src/index.js file. The origin and methods are now set based on the CORS_ORIGIN and CORS_METHODS environment variables, respectively. Additionally, the frontend now includes the CORS_ORIGIN environment variable in the .env file.
    
    Co-authored-by: Anubhav

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:'https://collabdocsbyanubhav.vercel.app',
+}));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
